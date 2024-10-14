e23c0e1fa2b83f3a679aaf3a41893d7356443123
kalid-2024 <kalidmohammed236@gmail.com>
2024-10-14 14:03:24 +0300

    made some corrections

--- fetch-data.js
+++ fetch-data.js
@@ -1,7 +1,7 @@
 
 document.addEventListener('DOMContentLoaded', fetchUserData);
 
-const fetchUserData = async () => {
+async function fetchUserData(){
     const apiUrl = "https://jsonplaceholder.typicode.com/users";
     const dataContainer = document.getElementById("api-data");
     try{
