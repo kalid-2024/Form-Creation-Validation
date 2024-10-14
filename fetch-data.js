c548b04e1162b9a2427519142bd09d1077fdb1f5
kalid-2024 <kalidmohammed236@gmail.com>
2024-10-14 21:29:51 +0300

    resolved some syntax errors!

--- fetch-data.js
+++ fetch-data.js
@@ -1,21 +1,27 @@
 
 document.addEventListener('DOMContentLoaded', fetchUserData);
 
+
+
 async function fetchUserData(){
     const apiUrl = "https://jsonplaceholder.typicode.com/users";
     const dataContainer = document.getElementById("api-data");
     try{
         const response = await fetch(apiUrl);
+        console.log(response);
         const users = await response.json();
+       
 
         dataContainer.innerHTML = '';
-        const userList  = document.creatElement("ul");
+        const userList  = document.createElement("ul");
+
         users.forEach((user) => {
             const li = document.createElement("li");
             li.textContent = user.name;
             userList.appendChild(li);
         });
         dataContainer.appendChild(userList);
+       
     }
     catch(error){
         dataContainer.innerHTML = "";
