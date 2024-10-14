c8d53e3f2e50c5ad945f94e830e190e610de0207
kalid-2024 <kalidmohammed236@gmail.com>
2024-10-14 21:39:13 +0300

    corrected logical errors,regex on the email validation and  typos

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
+
+document.addEventListener('DOMContentLoaded' , function () {
 
 const form = document.getElementById("registration-form");
 const feedbackDiv = document.getElementById("form-feedback");
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit' , (event) => {
 event.preventDefault();
 
 const username = document.getElementById("username").value.trim();
@@ -15,9 +16,9 @@ let messages = [];
 
 if (username.length < 3){
     isValid = false ;
-    messages.push("Error! Username  must be greater then 3 characters!") ;
-
-if(!/[@ .]/.test(email)) {
+    messages.push("Error! Username  must be greater than 3 characters!") ;
+    }
+if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
     isValid = false ;
     messages.push("Error! Email does not contain '@'  and '.' special characters!");
 }
@@ -26,20 +27,20 @@ if(password.length < 8){
     isValid = false ;
     messages.push("Password length should be at least 8 characters!");
 }
+
 // feedback message 
 
 feedbackDiv.style.display = "block";
 if (isValid){
     feedbackDiv.textContent = "Registration successful!";
     feedbackDiv.style.color = "#28a745";
+    form.submit();
 }else{
     feedbackDiv.innerHTML = messages.join("<br>");
     feedbackDiv.style.color = "#dc3545";
 }
 
-form.submit();
 
-
-});
+})
 
 });
