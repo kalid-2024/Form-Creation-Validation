a966c36ab5c111391f64ae52d86544a7def3d1b6
kalid-2024 <kalidmohammed236@gmail.com>
2024-10-14 13:16:29 +0300

    added index.html , style.css and script.js files

---
+++ script.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+const form = document.getElementById("registration-form");
+const feedbackDiv = document.getElementById("form-feedback");
+
+form.addEventListener('submit', (event) => {
+event.preventDefault();
+
+const username = document.getElementById("username").value.trim();
+const email = document.getElementById("email").value.trim();
+const password = document.getElementById("password").value.trim();
+
+let isValid = true ;
+let messages = []; 
+
+if (username.length < 3){
+    isValid = false ;
+    messages.push("Error! Username  must be greater then 3 characters!") ;
+
+if(!/[@ .]/.test(email)) {
+    isValid = false ;
+    messages.push("Error! Email does not contain '@'  and '.' special characters!");
+}
+
+if(password.length < 8){
+    isValid = false ;
+    messages.push("Password length should be at least 8 characters!");
+}
+// feedback message 
+
+feedbackDiv.style.display = "block";
+if (isValid){
+    feedbackDiv.textContent = "Registration successful!";
+    feedbackDiv.style.color = "#28a745";
+}else{
+    feedbackDiv.innerHTML = messages.join("<br>");
+    feedbackDiv.style.color = "#dc3545";
+}
+
+form.submit();
+
+
+});
+
+});
